14f4a332e4d12c78d2f68b86f1dc44508bb96fcd
Julien Pillaud <julien.pillaud@gmail.com>
2024-11-25 20:24:50 +0100

    add fastapi endpoint to test

--- app/api/main.py
+++ app/api/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 
-from app.config import settings
-
 api = FastAPI(
-    docs_url="/api/docs" if settings.ENVIRONMENT == "development" else None,
-    redoc_url=None,
-    openapi_url="/api/openapi.json" if settings.ENVIRONMENT == "development" else None,
-    prefix="/api",
+    openapi_url=None,
+    root_path="/api",
 )
 
 
