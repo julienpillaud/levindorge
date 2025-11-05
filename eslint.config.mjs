c713439df9ed8cc5cf0773bbefc7cdc2db4e2541
Julien Pillaud <julien.pillaud@gmail.com>
2025-11-05 11:16:57 +0100

    feat: update ESLint and globals dependencies, add ESLint configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
 ]);
