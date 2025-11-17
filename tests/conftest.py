9c7369854f1338c72c7b9c137a7015fd1e11bc92
Julien Pillaud <julien.pillaud@gmail.com>
2025-11-17 14:08:35 +0100

    feat: add github action

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,14 @@ pytest_plugins = [
 @pytest.fixture(scope="session")
 def settings() -> Settings:
     return Settings(
+        app_version="",
+        environment="test",
+        secret_key="",
         mongo_user="user",
         mongo_password="password",
         mongo_host="localhost",
         mongo_port=27017,
         mongo_database="test",
+        redis_host="localhost",
+        redis_port=6379,
     )
