9ea8208e7209ba1b387ce3989bf56726be805ed6
Julien Pillaud <julien.pillaud@gmail.com>
2025-12-11 16:44:41 +0100

    feat: add checkbox management for articles selection and price tags integration

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,29 @@
-import { buildCreateDropdownMenu } from "./menu.js";
-import { initArticles } from "./articles/init.js";
-import { initSearch } from "./search.js";
+import {buildCreateDropdownMenu} from "./menu.js";
+import {initArticles} from "./articles/init.js";
+import {initSearch} from "./search.js";
+import {initializeCheckboxes, updateCheckedArticleIds, updateDropdownVisibility} from "./checkboxes.js";
+import {showSelectedArticles, initializePriceTagsDropdown} from "./price-labels.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initSearch();
   initArticles();
   buildCreateDropdownMenu();
+
+  // ---------------------------------------------------------------------------
+  // Articles table checkboxes
+  initializeCheckboxes();
+
+  document.addEventListener("change", (event) => {
+    if (event.target.matches("table tbody .checkbox")) {
+      updateCheckedArticleIds(event);
+      updateDropdownVisibility();
+    }
+  });
+  // ---------------------------------------------------------------------------
+  const priceTagsSelected = document.getElementById("price-labels-selected")
+  priceTagsSelected.addEventListener("click", () => {
+    showSelectedArticles();
+  });
+
+  initializePriceTagsDropdown();
 });
---
+++ app/static/js/price-labels.js
@@ -0,0 +1,30 @@
+import {getCheckedArticleIds} from "./checkboxes.js";
+import {updateArticlesPage} from "./utils.js";
+import {getStores} from "./cache/stores.js";
+
+// -----------------------------------------------------------------------------
+export const showSelectedArticles = async () => {
+  const ids = getCheckedArticleIds();
+  const params = new URLSearchParams();
+
+  ids.forEach(id => params.append("article_ids", id));
+  const url = `/articles/ids?${params.toString()}`;
+
+  updateArticlesPage(url);
+}
+
+// -----------------------------------------------------------------------------
+export const initializePriceTagsDropdown = async () => {
+  const dropdown = document.getElementById("price-labels-dropdown");
+  const ul = dropdown.querySelector("ul");
+
+  const stores = await getStores();
+  stores.forEach(store => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.dataset.slug = store.slug;
+    a.textContent = store.name;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+}
